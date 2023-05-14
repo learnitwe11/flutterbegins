b6561ab96f8a4dbe6d36bc63b46f97e9be14d518
quinny <quinny.jtd@gmail.com>
2023-05-13 22:46:37 -0500

    added slid-in effect for register page

--- lib/pages/authenticate/register.dart
+++ lib/pages/authenticate/register.dart
@@ -10,15 +10,51 @@ class RegisterUser extends StatefulWidget {
   State<RegisterUser> createState() => _RegisterUserState();
 }
 
-class _RegisterUserState extends State<RegisterUser> {
+class _RegisterUserState extends State<RegisterUser> with SingleTickerProviderStateMixin{
 
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form
   final GlobalKey  _formKey = GlobalKey<FormState>();
+  late final AnimationController _controller;
+  late final Animation<Offset> _offsetAnimation;
+
 
   // text field state
   String email = "";
+  int emailCharCount = 0;
   String password = "";
+  int pwdCharCount = 0;
+
+  @override
+  void initState() {
+    
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds:1000),
+      vsync: this,
+    );
+
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(1, 0), end: const Offset(0, 0)
+    ).animate(CurvedAnimation(
+      curve: Curves.decelerate,
+      parent: _controller,
+      )
+     );
+
+     _controller.forward();
+
+
+  }
+
+  
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,55 +90,72 @@ class _RegisterUserState extends State<RegisterUser> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height:20.0),
-              TextFormField(
-                autocorrect: true,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.email),
-                  hintText: 'email address',
-                  counterText: '0 characters',
-                  border: OutlineInputBorder(),
-                  
-                ),
-                validator: (value){
-                  if(value == null || value.isEmpty){
-                    return 'Please enter some text';
-                  }else{
-                    return null;
-                  }
-                },
-                onChanged: (value){
-                  setState(()=> email = value);
-                  print(email);
-                },
-
-
-              ),
-              const SizedBox(height:20.0),
-              TextFormField(
-                autocorrect: true,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.password),
-                  hintText: 'password',
-                  counterText: '0 characters',
-                  border: OutlineInputBorder(),
-                  
-                ),
-                validator: (value){
-                  if(value ==null || value.isEmpty){
-                    return 'please enter a valid pwd';
-                  }else{
-                    if(value.length < 6){
-                      return 'pwd must be 6+ characters long';
+              SlideTransition(
+                position: _offsetAnimation,
+                child: TextFormField(
+                  autocorrect: true,
+                  decoration: const InputDecoration(
+                    icon: Icon(Icons.email),
+                    hintText: 'email address',
+                    //counterText: emailCharCount.toString(),
+                    //errorText: 'invalid email address',
+                    border: OutlineInputBorder(),
+                    
+                  ),
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return 'Please enter some text';
                     }else{
                       return null;
                     }
+                  },
+                  onChanged: (value){
+                    setState((){
+                      email = value;
+                      emailCharCount = email.length;
+                    } );
+                    //print(email);
+                  },
+              
+              
+                ),
+              ),
+              const SizedBox(height:20.0),
+              SlideTransition(
+                position: _offsetAnimation,
+
+                child: TextFormField(
+                  autocorrect: true,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    icon: const Icon(Icons.password),
+                    hintText: 'password',
+                    counterText: '${pwdCharCount.toString()} characters',
+                    //errorText: 'invalid password',
+                    border: const OutlineInputBorder(),
+                    
+                  ),
+                  validator: (value){
+                    if(value ==null || value.isEmpty){
+                      return 'please enter a valid pwd';
+                    }else{
+                      if(value.length < 6){
+                        return 'pwd must be 6+ characters long';
+                      }else{
+                        return null;
+                      }
+                    }
+                  },
+                  onChanged: (val){
+                    setState((){
+                      password = val;
+                      pwdCharCount = password.length;
+                      
+                    } );
+
+                    //print(password);
                   }
-                },
-                onChanged: (val){
-                  setState(()=> password = val);
-                  print(password);
-                }
+                ),
               ),
 
             ],
